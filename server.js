6f8311eb25d0bb7ed6100b535a1085b7583c6033
Josh Murray <joshmurray@Joshs-MBP.lan>
2022-11-08 06:29:32 -0800

    project init and set up env and db connect

---
+++ server.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+require('dotenv').config()
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'catalina',
+  database: 'employee_tracker'
+});
+
+connection.connect((err) => {
+    if(err) {
+        console.log('db connect err ===', err);
+    }
+    console.log('database connection ok ===');
+})
