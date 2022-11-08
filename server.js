08300b88220b805e79947a22e2c69fc89e5ec76f
Josh Murray <joshmurray@Joshs-MBP.lan>
2022-11-08 08:37:20 -0800

    added add department function

--- server.js
+++ server.js
@@ -1,63 +1,63 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-require('dotenv').config()
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+require("dotenv").config();
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'catalina',
-    database: 'employee_tracker'
+    host: "localhost",
+    user: "root",
+    password: "catalina",
+    database: "employee_tracker",
 });
 
 connection.connect((err) => {
     if (err) {
-        console.log('db connect err ===', err);
+        console.log("db connect err ===", err);
     }
-    console.log('database connection ok ===');
+    console.log("database connection ok ===");
     setTimeout(() => {
-        console.log('welcome to my Employee Tracker program!');
+        console.log("welcome to my Employee Tracker program!");
         start();
     }, 1000);
-
 });
 
 function start() {
-    inquirer.prompt({
-        type: 'list',
-        name: 'home_menu',
-        message: 'What would you like to do?',
-        choices: [
-            'View all Departments',
-            'View all Roles',
-            'View all Employees',
-            'Add Role',
-            'Add Department',
-            'Add Employee',
-            'Quit'
-        ]
-    }).then(
-        ({ home_menu }) => {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "home_menu",
+            message: "What would you like to do?",
+            choices: [
+                "View all Departments",
+                "View all Roles",
+                "View all Employees",
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Quit",
+            ],
+        })
+        .then(({ home_menu }) => {
             switch (home_menu) {
-                case 'View all Departments':
+                case "View all Departments":
                     viewAllDepartments();
                     break;
-                case 'View all Roles':
+                case "View all Roles":
                     viewAllRoles();
                     break;
-                case 'View all Employees':
+                case "View all Employees":
                     viewAllEmployees();
                     break;
-                case 'Add Department':
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
                     viewAllDepartments();
                     break;
-                case 'Add Role':
+                case "Add Employee":
                     viewAllDepartments();
                     break;
-                case 'Add Employee':
-                    viewAllDepartments();
-                    break;
-                case 'Quit':
+                case "Quit":
                     quit();
                     break;
 
@@ -65,8 +65,7 @@ function start() {
                     quit();
                     break;
             }
-        }
-    )
+        });
 }
 
 function restart() {
@@ -79,22 +78,22 @@ function viewAllDepartments() {
     const sql = `SELECT id, name FROM department`;
     connection.query(sql, (err, res) => {
         if (err) {
-            console.log('viewAllDepartments error ===', err);
+            console.log("viewAllDepartments error ===", err);
         }
         console.table(res);
         restart();
-    })
+    });
 }
 
 function viewAllRoles() {
     const sql = `SELECT role.id, role.title, role.salary, department.name FROM role, department WHERE role.department_id = department.id;`;
     connection.query(sql, (err, res) => {
         if (err) {
-            console.log('viewAllRoles error ===', err);
+            console.log("viewAllRoles error ===", err);
         }
         console.table(res);
         restart();
-    })
+    });
 }
 
 function viewAllEmployees() {
@@ -121,17 +120,49 @@ function viewAllEmployees() {
                     m.id = e.manager_id`;
     connection.query(sql, (err, res) => {
         if (err) {
-            console.log('viewAllEmployees error ===', err);
+            console.log("viewAllEmployees error ===", err);
         }
         console.table(res);
         restart();
-    })
+    });
+}
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: "text",
+                name: "newDepartment",
+                message: "Enter new department name:",
+            },
+        ])
+        .then(({ newDepartment }) => {
+            const sql = `INSERT INTO department (name) VALUES (?)`;
+            const value = newDepartment;
+            connection.query(sql, value, (err, res) => {
+                if (err) {
+                    console.log("addDepartment error ===", err);
+                }
+                inquirer
+                    .prompt({
+                        type: "confirm",
+                        name: "result",
+                        message: "check added department",
+                    })
+                    .then(({ result }) => {
+                        if (result) {
+                            viewAllDepartments();
+                        } else {
+                            restart();
+                        }
+                    });
+            });
+        });
 }
 
 function quit() {
-    console.log('Thank you. See you again.');
+    console.log("Thank you. See you again.");
     setTimeout(() => {
         process.exit(1);
     }, 1000);
 }
-
